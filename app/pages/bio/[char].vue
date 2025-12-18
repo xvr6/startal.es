125372d8f6bdd5f139e66285204b13ac5cf07bc3
Xavier <xvr0612@gmail.com>
2025-12-17 19:10:00 -0500

    notes

--- app/pages/bio/[char].vue
+++ app/pages/bio/[char].vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 const route = useRoute();
-const who = route.params.char;
+const who = route.params.char?.toString();
 
 console.log(who);
 </script>
@@ -9,6 +9,7 @@ console.log(who);
   <UContainer class="mt-4">
     <p>{{ who }}</p>
   </UContainer>
+  <!-- use new layout tupe for other character pages instead of this stupid dynamic component thing  -->
 </template>
 
 <style></style>
---
+++ app/pages/bio/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <UContainer>
+    <div>index char</div>
+  </UContainer>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
